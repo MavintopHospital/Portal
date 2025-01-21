4240435b5122228189010791a1d8dd538b0582b9
rajeevbn28 <rajeevbn28@gmail.com>
2025-01-22 01:16:07 +0530

    Ui Fix
    few ui changes

--- src/Components/Main/Services/Services.tsx
+++ src/Components/Main/Services/Services.tsx
@@ -8,7 +8,7 @@ import SectionWrapper from '../SectionWrapper';
 import Appointment from '@/Components/Main/Appointment/Appointment';
 
 const Services = () => {
-  const [SelectService, setSelectService] = useState<SelectedService>(
+  const [selectedService, setSelectedService] = useState<SelectedService>(
     SelectedService.Inpatient
   );
   const [isAppointmentOpen, setAppointmentOpen] = useState(false);
@@ -17,35 +17,39 @@ const Services = () => {
   const closeAppointment = () => setAppointmentOpen(false);
 
   const selectedServiceData = ServicesData.find(
-    (service) => service.id === SelectService
+    (service) => service.id === selectedService
   );
+
   return (
     <SectionWrapper id="services">
       <h2 className="text-4xl font-bold text-center mb-10">Services</h2>
-      <div className="flex flex-col justify-between lg:flex-row gap-10 lg:gap-5">
-        <div className="grid grid-cols-2 gap-5">
+      <div className="flex flex-col lg:flex-row gap-10 lg:gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-6">
           {ServicesData?.map((service, index) => (
             <Service
               key={index}
               service={service}
-              SelectService={SelectService}
-              setSelectService={setSelectService}
+              SelectService={selectedService}
+              setSelectService={setSelectedService}
             />
           ))}
         </div>
-        <div className="lg:min-w-[30rem] lg:max-w-[30rem]">
-          <h3 className="text-xl xs:text-2xl font-bold mb-4">
+        <div className="lg:min-w-[30rem] lg:max-w-[30rem] mt-8 lg:mt-0">
+          <h3 className="text-2xl font-bold mb-4">
             {selectedServiceData?.heading}
           </h3>
           <div className="lg:ml-5 min-h-[33.4375rem]">
-            <ul className="grid grid-cols-1 gap-4 md:gap-6">
+            <ul className="grid grid-cols-1 gap-4 md:gap-6 mb-6">
               {selectedServiceData?.texts.map((text, index) => (
                 <ListItem key={index} text={text} />
               ))}
             </ul>
-          </div>
-          <div className="lg:ml-5 mt-1">
-          <button onClick={openAppointment} className="mt-4 p-2 bg-blue-500 text-white rounded">Appointment Now</button>
+            <button
+              onClick={openAppointment}
+              className="px-6 py-3 bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition-all duration-300"
+            >
+              Book Appointment
+            </button>
           </div>
         </div>
       </div>
